14ab8e432f8274dc54b4c60a4c65853b14b31856
jchia030 <55873474+jchia030@users.noreply.github.com>
2019-09-27 19:30:48 +0800

    Update and rename Homework2.py to p2-2.py

--- 02/Homework2.py
+++
@@ -1,6 +0,0 @@
-def convert_fahrenheit(temp):
-    return (float(temp)*1.8 + 32)
-
-c=input('Pls enter temperature in Celsius:')
-
-print('The temperate in Faherenheit is {}'.format(convert_fahrenheit(c)))
---
+++ 02/p2-2.py
@@ -0,0 +1,3 @@
+temp=input('Pls enter temperature in Celsius:')
+
+print('The temperature in Faherenheit is ',(float(temp)*1.8+32))
