73492e0c946c652117fc8708ca922dfc937bc66f
jchia030 <55873474+jchia030@users.noreply.github.com>
2019-10-04 20:48:03 +0800

    Update C1.py

--- 04/C1.py
+++ 04/C1.py
@@ -1,7 +1,7 @@
 min_list=[9,41,12,3,74,15]
 min_value=min_list[0]
 for i in range(1,len(min_list)):
-    if min_list[i]<min_list[i-1]:
+    if min_list[i]<min_value:
         min_value=min_list[i]
 print(min_value)
         
