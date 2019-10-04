12ea3be44be59cf9940d471fa1c03775f563603f
jchia030 <55873474+jchia030@users.noreply.github.com>
2019-10-04 20:33:45 +0800

    Update C2.py

--- 04/C2.py
+++ 04/C2.py
@@ -20,7 +20,7 @@ def average(ls):
     print(average_value)
     
 def median(ls):
-    ls=sorted(ls,ascending=True)
+    ls=sorted(ls)
     if len(ls)%2==0:
           i=int(len(ls)/2)
           median_value=(ls[i]+ls[i+1])/2
