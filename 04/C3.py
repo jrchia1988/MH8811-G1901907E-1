016602a5102f235890f50fd2217bc6543fc18c17
jchia030 <55873474+jchia030@users.noreply.github.com>
2019-10-04 19:43:15 +0800

    Update C3.py

--- 04/C3.py
+++ 04/C3.py
@@ -7,14 +7,12 @@ def getFileLines(fname):
                   lines.append(line)
       return lines
       
-filename='https://gist.githubusercontent.com/bazzilic/ebea5d4bcfd786530f43c255f2b1b14b/raw/ad0392386cf52a61932972e26e22d7a28f4091a6/MH8811-04-Data.csv'
+getFileLines('MH8811-04-Data.csv')
 
-getFileLines(filename)
-
-from C2 import average,min,max,median,range
+from C2 import average,min_,max_,median,range_
 
 average(lines)
-min(lines)
-max(lines)
+min_(lines)
+max_(lines)
 median(lines)
-range(lines)
+range_(lines)
