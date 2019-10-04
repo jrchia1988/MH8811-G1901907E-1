b7d2aa656d0d0a04f2970228d70a193821af06ea
jchia030 <55873474+jchia030@users.noreply.github.com>
2019-10-04 19:37:23 +0800

    Update C2.py

--- 04/C2.py
+++ 04/C2.py
@@ -1,43 +1,41 @@
-def min(ls):
-    for i in range(1,len(ls)):
-        if ls[i]<ls[i-1]
+def min_(ls):
+    min_value=ls[0]
+    for i in range(1,len(ls)):    
+        if ls[i]<min_value:
             min_value=ls[i]
-        else:
-            min_value=ls[i-1]
-    return min_value
+    print(min_value)
     
-def max(ls):
+def max_(ls):
+    max_value=ls[0]
     for i in range(1,len(ls)):
-        if ls[i]>ls[i-1]
+        if ls[i]>max_value:
             max_value=ls[i]
-        else:
-            max_value=ls[i-1]
-    return max_value
+    print(max_value)
     
 def average(ls):
-    sum=0
+    total_sum=0
     for i in range(len(ls)):
         total_sum+=i
     average_value=total_sum/len(ls)
-    return average_value
+    print(average_value)
     
 def median(ls):
     if len(ls)%2==0:
-          i=len(ls)/2
+          i=int(len(ls)/2)
           median_value=(ls[i]+ls[i+1])/2
     else:
           i=(len(ls)+1)/2
           median_value=ls[i]
-    return median_value
+    print(median_value)
     
-def range(ls):
-    return max(ls)-min(ls)
+def range_(ls):
+    print(max(ls)-min(ls))
     
 c1_list=[9,41,12,3,74,15]
 
-min(c1_list)
-max(c1_list)
+min_(c1_list)
+max_(c1_list)
 average(c1_list)
 median(c1_list)
-range(c1_list)
+range_(c1_list)
         
