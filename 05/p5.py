02047b816d5c88423240bf96f65475cb07ecc318
jchia030 <55873474+jchia030@users.noreply.github.com>
2019-10-11 20:32:55 +0800

    Update p5.py

--- 05/p5.py
+++ 05/p5.py
@@ -2,18 +2,24 @@
 
 import pickle
 
-def serialize():
-    user_list=[]
-    while ans!='q':
-         ans=input('Please key in an integer:')
-         try:
-            val=int(ans)
-         except:
-            ans='q'
-            print('Pls try again.')
-         user_list.append(val)
-    serial=input('Pls type in filename.')
-    serial=pickle.dumps(user_list)
-    return serial
+def serialize(lst):
+    string=''
+    for i in lst:
+        string+=i
+    file=input('Pls type in filename:')
+    file=pickle.dumps(string)
+    print(pickle.load(file)) 
+    
+def deserialize(string):
+    lst=[]
+    for i in string.split():
+        lst.append(i)
+    return lst
+
+def compare(lst1,lst2):
+    if lst1==lst2:
+        print('Success!')
+        
+    
          
          
