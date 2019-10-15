4c2630fcfb23849f8b3c0b877b86591f57df5808
jchia030 <55873474+jchia030@users.noreply.github.com>
2019-10-16 00:13:41 +0800

    Update p5.py

--- 05/p5.py
+++ 05/p5.py
@@ -2,55 +2,86 @@
 
 import pickle
 
-lst=[1,2,3,4,5]
-
 def serialize(lst):
-    string=''
-    for i in lst:
-        string+=i
-    string='|'.join(string)
-    return string
+    if type(lst)==list:
+        string='list|'
+        for i in lst:
+            string+=str(i)
+            string+='|'
+            string+=str(type(i))
+            string+='|'
+        return string[:-1]
+    elif type(lst)==dict:
+        string='dict|'
+        for i,v in lst.items():
+            string+=str(i)
+            string+='|'
+            string+=str(type(i))
+            string+='|'
+            string+=str(v)
+            string+='|'
+            string+=str(type(v))
+            string+='|'
+        return string
     
 def deserialize(string):
-    lst=[]
-    for i in string.split('|'):
-        lst.append(int(i))
-    return lst
+    a=string.split('|')
+    if a[0]=='list':
+        lst=[]
+        for i in range(1,len(a)-1,2):
+            if 'float' in a[i+1]:
+                lst.append(float(a[i]))
+            elif 'int' in a[i+1]:
+                lst.append(int(a[i]))
+            else:
+                lst.append(a[i])
+        return lst
+    else:
+        dct={}
+        for i in range(1,len(a)-1,4):
+            if 'float' in a[i+1]:
+                b=float(a[i])
+            elif 'int' in a[i+1]:
+                b=int(a[i])
+            else:
+                b=a[i]
+            if 'float' in a[i+3]:
+                c=float(a[i+2])
+            elif 'int' in a[i+3]:
+                c=int(a[i+2])
+            else:
+                c=a[i+2]
+            dct[b]=c
+        return dct
+        
+def compare(lst1,lst2):
+    if type(lst1)==type(lst2):
+        if lst1==lst2:
+            print('Success!')
+        else:
+            print('Fail')
+    else:
+        print('Fail')
 
-def compare(lst):
-    lst1=deserialize(lst)
-    if lst==lst1:
-        print('Success!')
-
-serialized=serialize(lst)        
-file=input('Pls type in filename:')
-filename=file+'.pkl'
-a=open(filename,'wb')
-pickle.dump(serialized,a)
-a.close()
-b=open(filename,'rb')
-serialized=pickle.load(file,b)
-
-compare(serialized)
 
 #Homework
 
 import json
-file=input('Pls select file: H1, H2, H3, H4, H5')
+file=input('Pls select file: H1-1,H1-2,H1-3,H1-4,H1-5.')
 filename=file+'.json'
-fh=open(filename)
-data=json.load(fh)
+with open(filename,'r') as fh:
+    data=json.load(fh)
 fh.close()
 serialized=serialize(data)
 file=input('Pls type in filename:')
 filename=file+'.pkl'
-a=open(filename,'wb')
-pickle.dump(serialized,a)
+with open(filename,'w') as a:
+    pickle.dump(serialized,a)
 a.close()
-b=open(filename,'rb')
-serial=pickle.load(b)
-if deserialize(serial)==data:
-    print('Success!')
+with open(filename,'r') as b:
+    serial=pickle.load(b)
+
+compare(data,serial)
 
 
          
